7adefd6a0d6cd30742aaf96a79ab25a2171c3370
Carsten Binnig <carsten.binnig@brown.edu>
2016-03-25 17:32:08 +0100

    Added complex query

--- src/org/xdb/elasticity/queries/TPCHQuery99.java
+++ src/org/xdb/elasticity/queries/TPCHQuery99.java
@@ -16,33 +16,49 @@ public class TPCHQuery99 extends DistributedQuery {
 		try {
 			PartitionTable oldTable = this.getPartitionTable();
 			PartitionTable newTable = new PartitionTable();
+			PartitionTable lineitemParts = catalog.get("lineitem");
 			PartitionTable ordersParts = catalog.get("orders");
 			PartitionTable customerParts = catalog.get("customer");
+			PartitionTable suppParts = catalog.get("supplier");
+			PartitionTable partParts = catalog.get("part");
 			PartitionTable nationParts = catalog.get("nation");
 			PartitionTable regionParts = catalog.get("region");
 
-			for (String partition : ordersParts.partitions()) {
+			for (String partition : lineitemParts.partitions()) {
 				boolean update = true;
 				
+				String lineitemConn = lineitemParts.getConnection(partition)
+						.getMetaData().getURL();
 				String ordersConn = ordersParts.getConnection(partition)
 						.getMetaData().getURL();
 				String customerConn = customerParts.getConnection(partition)
+						.getMetaData().getURL();
+				String suppConn = suppParts.getConnection(partition)
+						.getMetaData().getURL();
+				String partConn = partParts.getConnection(partition)
 						.getMetaData().getURL();
 				String nationConn = nationParts.getConnection(partition)
 						.getMetaData().getURL();
 				String regionConn = regionParts.getConnection(partition)
 						.getMetaData().getURL();
 				
-				if (!ordersConn.equals(customerConn)) {
+				if (!lineitemConn.equals(ordersConn)) {
 					update = false;
-				} else if (!ordersConn.equals(nationConn)) {
+				} else if (!lineitemConn.equals(customerConn)) {
 					update = false;
-				} else if (!ordersConn.equals(regionConn)) {
+				} else if (!lineitemConn.equals(suppConn)) {
+					update = false;
+				} else if (!lineitemConn.equals(partConn)) {
+					update = false;
+				}
+				else if (!lineitemConn.equals(nationConn)) {
+					update = false;
+				} else if (!lineitemConn.equals(regionConn)) {
 					update = false;
 				}
 				
 				if(update){
-					newTable.update(partition, ordersParts.getConnection(partition));
+					newTable.update(partition, lineitemParts.getConnection(partition));
 				}
 				else{
 					newTable.update(partition, oldTable.getConnection(partition));
@@ -58,13 +74,19 @@ public class TPCHQuery99 extends DistributedQuery {
 	}
 
 	private static String Q99 = "select "
+			+ "p_name, "
 			+ "n_name, "
-			+ "count(*) as total "
+			+ "sum(l_extendedprice * (1-l_discount)) as revenue "
 			+ "from  customer_<P>, orders_<P>, "
+			+ "lineitem_<P>, supplier_<P>, part_<P>, "
 			+ "nation_<P>, region_<P> "
 			+ "where c_custkey = o_custkey "
-			+ "and c_nationkey = n_nationkey "
-			+ "and r_regionkey = n_regionkey " 
-			+ "and r_name = 'ASIA' "
-			+ "group by n_name;";
+			+ "and l_orderkey = o_orderkey " 
+			+ "and l_partkey = p_partkey " 
+			+ "and l_suppkey = s_suppkey "
+			+ "and c_nationkey = s_nationkey "
+			+ "and s_nationkey = n_nationkey "
+			+ "and r_regionkey = n_regionkey " + "and r_name = 'ASIA' "
+			+ "and o_orderdate > DATE('1994-01-01')  "
+			+ "and o_orderdate < DATE('1995-01-01') " + "group by p_name, n_name;";
 }
